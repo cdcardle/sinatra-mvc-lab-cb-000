54392fc106720b57b9b1c239cc7bba20031f6d7d
Chris Cardle <cdcardle@gmail.com>
2018-03-05 01:37:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @user_phrase = PigLatinizer.new(params[:user_input])
+    @user_phrase = PigLatinizer.new(params[:user_phrase])
 
     erb :output
   end
